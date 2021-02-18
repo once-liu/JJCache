e340aa90863815198d6afd4016a6361e6dfe90e4
lzc <zuchuang.liu@melot.cn>
2021-02-18 15:03:55 +0800

    create memorycache

---
+++ JJCache/JJCache/JJMemoryCache.h
@@ -0,0 +1,16 @@
+//
+//  JJMemoryCache.h
+//  JJCache
+//
+//  Created by melot on 2021/2/18.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface JJMemoryCache : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ JJCache/JJCache/JJMemoryCache.m
@@ -0,0 +1,12 @@
+//
+//  JJMemoryCache.m
+//  JJCache
+//
+//  Created by melot on 2021/2/18.
+//
+
+#import "JJMemoryCache.h"
+
+@implementation JJMemoryCache
+
+@end
