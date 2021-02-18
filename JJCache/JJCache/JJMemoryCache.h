1c3d6e3b9c8b14ac266a6dea5ecca8b2b72d573d
lzc <zuchuang.liu@melot.cn>
2021-02-18 17:45:03 +0800

    JJMemoryCache Implementation

--- JJCache/JJCache/JJMemoryCache.h
+++ JJCache/JJCache/JJMemoryCache.h
@@ -11,6 +11,60 @@ NS_ASSUME_NONNULL_BEGIN
 
 @interface JJMemoryCache : NSObject
 
+@property (nullable, copy) NSString *name;
+
+@property (readonly) NSUInteger totalCount;
+
+@property (readonly) NSUInteger totalCost;
+
+
+//MARK: - Limit
+
+@property NSUInteger costLimit;
+
+@property NSUInteger countLimit;
+
+@property NSTimeInterval ageLimit;
+
+@property NSTimeInterval autoTrimInterval;
+
+@property BOOL shouldRemoveAllObjectsOnMemoryWarning;
+
+@property BOOL shouldRemoveAllObjectsWhenEnteringBackground;
+
+@property (nonatomic, copy) void(^didReceiveMemoryWarningBlock)(JJMemoryCache *cache);
+
+@property (nonatomic, copy) void(^didEnterBackgroundBlock)(JJMemoryCache *cache);
+
+@property BOOL releaseOnMainThread;
+
+@property BOOL releaseAsynchronously;
+
+
+//MARK: - Access Methods
+
+- (BOOL)containsObjectForKey:(id)key;
+
+- (nullable id)objectForKey:(id)key;
+
+- (void)setObject:(nullable id)object forKey:(id)key;
+
+- (void)setObject:(nullable id)object forKey:(id)key withCost:(NSUInteger)cost;
+
+- (void)removeObjectForKey:(id)key;
+
+- (void)removeAllObjects;
+
+
+//MARK: - Trim
+
+- (void)trimToCount:(NSUInteger)count;
+
+- (void)trimToCost:(NSUInteger)cost;
+
+- (void)trimToAge:(NSTimeInterval)age;
+
+
 @end
 
 NS_ASSUME_NONNULL_END
--- JJCache/JJCache/JJMemoryCache.m
+++ JJCache/JJCache/JJMemoryCache.m
@@ -6,7 +6,501 @@
 //
 
 #import "JJMemoryCache.h"
+#import <UIKit/UIKit.h>
+#import <pthread/pthread.h>
 
-@implementation JJMemoryCache
+
+static inline dispatch_queue_t JJMemoryCacheGetReleaseQueue() {
+    return dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0);
+}
+
+
+// 使用链表存储。Node 为最小存储单元
+@interface _JJLinkedMapNode : NSObject {
+    @package
+    __unsafe_unretained _JJLinkedMapNode *_prev;
+    __unsafe_unretained _JJLinkedMapNode *_next;
+    
+    id _key;
+    id _value;
+    NSUInteger _cost;
+    NSTimeInterval _time;
+}
+
+@end
+
+@implementation _JJLinkedMapNode
+
+@end
+
+
+// 链表
+@interface _JJLinkedMap : NSObject {
+    @package
+    CFMutableDictionaryRef _dic;
+    NSUInteger _totalCost;
+    NSUInteger _totalCount;
+    _JJLinkedMapNode *_head;
+    _JJLinkedMapNode *_tail;
+    BOOL _releaseOnMainThread;
+    BOOL _releaseAsynchronously;
+}
+
+- (void)insertNodeAtHead:(_JJLinkedMapNode *)node;
+
+- (void)bringNodeToHead:(_JJLinkedMapNode *)node;
+
+- (void)removeNode:(_JJLinkedMapNode *)node;
+
+- (_JJLinkedMapNode *)removeTialNode;
+
+- (void)removeAll;
+
+@end
+
+@implementation _JJLinkedMap
+
+- (void)dealloc {
+    CFRelease(_dic);
+}
+
+- (instancetype)init {
+    self = [super init];
+    
+    _dic = CFDictionaryCreateMutable(CFAllocatorGetDefault(), 0, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+    _releaseOnMainThread = NO;
+    _releaseAsynchronously = YES;
+    
+    return self;
+}
+
+- (void)insertNodeAtHead:(_JJLinkedMapNode *)node {
+    CFDictionarySetValue(_dic, (__bridge const void *)(node->_key), (__bridge const void *)(node));
+    _totalCost += node->_cost;
+    _totalCost++;
+    if (_head) {
+        node->_next = _head;
+        _head->_prev = node;
+        _head = node;
+    } else {
+        _head = _tail = node;
+    }
+}
+
+- (void)bringNodeToHead:(_JJLinkedMapNode *)node {
+    if (_head == node) return;
+    
+    if (_tail == node) {
+        _tail = node->_prev;
+        _tail->_next = nil;
+    } else {
+        node->_prev->_next = node->_next;
+        node->_next->_prev = node->_prev;
+    }
+    
+    node->_next = _head;
+    node->_prev = nil;
+    _head->_prev = node;
+    _head = node;
+}
+
+- (void)removeNode:(_JJLinkedMapNode *)node {
+    CFDictionaryRemoveValue(_dic, (__bridge const void *)(node->_key));
+    _totalCost -= node->_cost;
+    _totalCount--;
+    
+    if (node->_next) node->_next->_prev = node->_prev;
+    if (node->_prev) node->_prev->_next = node->_next;
+    if (_head == node) _head = node->_next;
+    if (_tail == node) _tail = node->_prev;
+}
+
+- (_JJLinkedMapNode *)removeTialNode {
+    if (!_tail) return nil;
+    
+    _JJLinkedMapNode *tail = _tail;
+    CFDictionaryRemoveValue(_dic, (__bridge const void *)(_tail->_key));
+    _totalCost -= _tail->_cost;
+    _totalCount--;
+    if (_head == _tail) {
+        _head = _tail = nil;
+    } else {
+        _tail = _tail->_prev;
+        _tail->_next = nil;
+    }
+    return tail;
+}
+
+- (void)removeAll {
+    _totalCost = 0;
+    _totalCount = 0;
+    _head = nil;
+    _tail = nil;
+    if (CFDictionaryGetCount(_dic) > 0) {
+        CFMutableDictionaryRef holder = _dic;
+        _dic = CFDictionaryCreateMutable(CFAllocatorGetDefault(), 0, &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+        
+        if (_releaseAsynchronously) {
+            dispatch_queue_t queue = _releaseOnMainThread ? dispatch_get_main_queue() : JJMemoryCacheGetReleaseQueue();
+            dispatch_async(queue, ^{
+                CFRelease(holder);
+            });
+        } else if (_releaseOnMainThread && !pthread_main_np()) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                CFRelease(holder);
+            });
+        } else {
+            CFRelease(holder);
+        }
+    }
+}
+
+@end
+
+
+
+
+@implementation JJMemoryCache {
+    pthread_mutex_t _lock;
+    _JJLinkedMap *_lru;
+    dispatch_queue_t _queue;
+}
+
+
+- (void)_trimInRecursively {
+    // 每隔 _autoTrimInterval 时间检测一次。超过限制则清理。
+    __weak typeof(self) weakSelf = self;
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(_autoTrimInterval *NSEC_PER_SEC)), dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_LOW, 0), ^{
+        __strong typeof(weakSelf) self = weakSelf;
+        if (!self) return;
+        [self _trimInBackground];
+        [self _trimInRecursively];
+    });
+}
+
+- (void)_trimInBackground {
+    dispatch_async(_queue, ^{
+        [self _trimToCost:self->_costLimit];
+        [self _trimToCount:self->_countLimit];
+        [self _trimToAge:self->_ageLimit];
+    });
+}
+
+- (void)_trimToCost:(NSUInteger)costLimit {
+    BOOL finish = NO;
+    pthread_mutex_lock(&_lock);
+    if (costLimit == 0) {
+        [_lru removeAll];
+        finish = YES;
+    }
+    pthread_mutex_unlock(&_lock);
+    if (finish) return;
+    
+    NSMutableArray *holder = [NSMutableArray new];
+    // 遍历移除最后一个 node，直到 lru->totalCost 小于 costLimit
+    while (!finish) {
+        if (pthread_mutex_trylock(&_lock)) {
+            if (_lru->_totalCost > costLimit) {
+                _JJLinkedMapNode *node = [_lru removeTialNode];
+                if (node) [holder addObject:node];
+            } else {
+                finish = YES;
+            }
+            pthread_mutex_unlock(&_lock);
+        } else {
+            usleep(10 * 1000);  // 10 ms
+        }
+    }
+    if (holder.count) {
+        dispatch_queue_t queue = _lru->_releaseOnMainThread ? dispatch_get_main_queue() : JJMemoryCacheGetReleaseQueue();
+        dispatch_async(queue, ^{
+            [holder count]; // release in queue
+        });
+    }
+}
+
+- (void)_trimToCount:(NSUInteger)countLimit {
+    BOOL finish = NO;
+    pthread_mutex_lock(&_lock);
+    if (countLimit == 0) {
+        [_lru removeAll];
+        finish = YES;
+    }
+    pthread_mutex_unlock(&_lock);
+    if (finish) return;
+    
+    NSMutableArray *holder = [NSMutableArray new];
+    // 遍历移除最后一个 node，直到 lru->totalCount 小于 countLimit
+    while (!finish) {
+        if (pthread_mutex_trylock(&_lock)) {
+            if (_lru->_totalCount > _countLimit) {
+                _JJLinkedMapNode *node = [_lru removeTialNode];
+                if (node) [holder addObject:node];
+            } else {
+                finish = YES;
+            }
+            pthread_mutex_unlock(&_lock);
+        } else {
+            usleep(10 * 1000);  // 10 ms
+        }
+    }
+    if (holder.count) {
+        dispatch_queue_t queue = _lru->_releaseOnMainThread ? dispatch_get_main_queue() : JJMemoryCacheGetReleaseQueue();
+        dispatch_async(queue, ^{
+            [holder count]; // release in queue
+        });
+    }
+}
+
+- (void)_trimToAge:(NSTimeInterval)ageLimit {
+    BOOL finish = NO;
+    NSTimeInterval now = CACurrentMediaTime();
+    pthread_mutex_lock(&_lock);
+    if (ageLimit <= 0) {
+        [_lru removeAll];
+        finish = YES;
+    } else if (!_lru->_tail || (now - _lru->_tail->_time) <= ageLimit) {
+        finish = YES;
+    }
+    pthread_mutex_unlock(&_lock);
+    
+    NSMutableArray *holder = [NSMutableArray new];
+    while (!finish) {
+        if (pthread_mutex_trylock(&_lock)) {
+            if (_lru->_tail && (now - _lru->_tail->_time) > ageLimit) {
+                _JJLinkedMapNode *node = [_lru removeTialNode];
+                if (node) [holder addObject:node];
+            } else {
+                finish = YES;
+            }
+            pthread_mutex_unlock(&_lock);
+        } else {
+            usleep(10 * 1000);  // 10 ms
+        }
+    }
+    if (holder.count) {
+        dispatch_queue_t queue = _lru->_releaseOnMainThread ? dispatch_get_main_queue() : JJMemoryCacheGetReleaseQueue();
+        dispatch_async(queue, ^{
+            [holder count]; // release in queue
+        });
+    }
+}
+
+- (void)_appDidReceiveMemoryWarningNotification {
+    if (self.didReceiveMemoryWarningBlock) {
+        self.didReceiveMemoryWarningBlock(self);
+    }
+    if (self.shouldRemoveAllObjectsOnMemoryWarning) {
+        [self removeAllObjects];
+    }
+}
+
+- (void)_appDidEnterBackgroundNotification {
+    if (self.didEnterBackgroundBlock) {
+        self.didEnterBackgroundBlock(self);
+    }
+    if (self.shouldRemoveAllObjectsWhenEnteringBackground) {
+        [self removeAllObjects];
+    }
+}
+
+
+//MARK: - Public
+
+- (void)dealloc {
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+    [_lru removeAll];
+    pthread_mutex_destroy(&_lock);
+}
+
+- (instancetype)init {
+    self = super.init;
+    
+    pthread_mutex_init(&_lock, NULL);
+    _lru = [_JJLinkedMap new];
+    _queue = dispatch_queue_create("com.jj.cache.memory", DISPATCH_QUEUE_SERIAL);
+    
+    _costLimit = NSUIntegerMax;
+    _countLimit = NSUIntegerMax;
+    _ageLimit = DBL_MAX;
+    _autoTrimInterval = 5.0;
+    _shouldRemoveAllObjectsOnMemoryWarning = YES;
+    _shouldRemoveAllObjectsWhenEnteringBackground = YES;
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_appDidReceiveMemoryWarningNotification) name:UIApplicationDidReceiveMemoryWarningNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(_appDidEnterBackgroundNotification) name:UIApplicationDidEnterBackgroundNotification object:nil];
+    
+    [self _trimInRecursively];
+    return self;
+}
+
+
+- (NSUInteger)totalCount {
+    pthread_mutex_lock(&_lock);
+    NSUInteger count = _lru->_totalCount;
+    pthread_mutex_unlock(&_lock);
+    return count;
+}
+
+- (NSUInteger)totalCost {
+    pthread_mutex_lock(&_lock);
+    NSUInteger totalCost = _lru->_totalCost;
+    pthread_mutex_unlock(&_lock);
+    return totalCost;
+}
+
+- (BOOL)releaseOnMainThread {
+    pthread_mutex_lock(&_lock);
+    BOOL releaseOnMainThread = _lru->_releaseOnMainThread;
+    pthread_mutex_unlock(&_lock);
+    return releaseOnMainThread;
+}
+
+- (void)setReleaseOnMainThread:(BOOL)releaseOnMainThread {
+    pthread_mutex_lock(&_lock);
+    _lru->_releaseOnMainThread = releaseOnMainThread;
+    pthread_mutex_unlock(&_lock);
+}
+
+- (BOOL)releaseAsynchronously {
+    pthread_mutex_lock(&_lock);
+    BOOL releaseAsynchronously = _lru->_releaseAsynchronously;
+    pthread_mutex_unlock(&_lock);
+    return releaseAsynchronously;
+}
+
+- (void)setReleaseAsynchronously:(BOOL)releaseAsynchronously {
+    pthread_mutex_lock(&_lock);
+    _lru->_releaseAsynchronously = releaseAsynchronously;
+    pthread_mutex_unlock(&_lock);
+}
+
+
+- (BOOL)containsObjectForKey:(id)key {
+    if (!key) return NO;
+    
+    pthread_mutex_lock(&_lock);
+    BOOL contains = CFDictionaryContainsKey(_lru->_dic, (__bridge const void *)(key));
+    pthread_mutex_unlock(&_lock);
+    return contains;
+}
+
+- (void)setObject:(id)object forKey:(id)key {
+    [self setObject:object forKey:key withCost:0];
+}
+
+- (void)setObject:(id)object forKey:(id)key withCost:(NSUInteger)cost {
+    if (!key) return;
+    if (!object) {
+        // 传入 value 为 nil，表示删除这个值
+        [self removeObjectForKey:key];
+        return;
+    }
+    
+    pthread_mutex_lock(&_lock);
+    _JJLinkedMapNode *node = CFDictionaryGetValue(_lru->_dic, (__bridge const void *)(key));
+    NSTimeInterval now = CACurrentMediaTime();
+    if (node) {
+        _lru->_totalCost -= node->_cost;
+        _lru->_totalCost += cost;
+        node->_cost = cost;
+        node->_time = now;
+        node->_value = object;
+        [_lru bringNodeToHead:node];
+    } else {
+        node = [_JJLinkedMapNode new];
+        node->_cost = cost;
+        node->_time = now;
+        node->_key = key;
+        node->_value = object;
+        [_lru insertNodeAtHead:node];
+    }
+    if (_lru->_totalCost > _costLimit) {
+        __weak typeof(self) weakSelf = self;
+        dispatch_async(_queue, ^{
+           __strong typeof(weakSelf) self = weakSelf;
+            [self trimToCost:self->_costLimit];
+        });
+    }
+    if (_lru->_totalCount > _countLimit) {
+        _JJLinkedMapNode *node = [_lru removeTialNode];
+        if (_lru->_releaseAsynchronously) {
+            dispatch_queue_t queue = _lru->_releaseOnMainThread ? dispatch_get_main_queue() : JJMemoryCacheGetReleaseQueue();
+            dispatch_async(queue, ^{
+                [node class];
+            });
+        } else if (_lru->_releaseOnMainThread && !pthread_main_np()) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [node class];
+            });
+        }
+    }
+    
+    pthread_mutex_unlock(&_lock);
+}
+
+- (id)objectForKey:(id)key {
+    if (!key) return nil;
+    
+    pthread_mutex_lock(&_lock);
+    _JJLinkedMapNode *node = CFDictionaryGetValue(_lru->_dic, (__bridge const void *)(key));
+    if (node) {
+        node->_time = CACurrentMediaTime();
+        [_lru bringNodeToHead:node];
+    }
+    pthread_mutex_unlock(&_lock);
+    
+    return node ? node->_value : nil;
+}
+
+- (void)removeObjectForKey:(id)key {
+    if (!key) return;
+    
+    pthread_mutex_lock(&_lock);
+    _JJLinkedMapNode *node = CFDictionaryGetValue(_lru->_dic, (__bridge const void *)(key));
+    if (node) {
+        [_lru removeNode:node];
+        if (_lru->_releaseAsynchronously) {
+            dispatch_queue_t queue = _lru->_releaseOnMainThread ? dispatch_get_main_queue() : JJMemoryCacheGetReleaseQueue();
+            dispatch_async(queue, ^{
+                [node class];
+            });
+        } else if (_lru->_releaseOnMainThread && !pthread_main_np()) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [node class];
+            });
+        }
+    }
+    pthread_mutex_unlock(&_lock);
+}
+
+- (void)removeAllObjects {
+    pthread_mutex_lock(&_lock);
+    [_lru removeAll];
+    pthread_mutex_unlock(&_lock);
+}
+
+
+- (void)trimToCount:(NSUInteger)count {
+    if (count == 0) {
+        [self removeAllObjects];
+        return;
+    }
+    [self _trimToCount:count];
+}
+
+- (void)trimToCost:(NSUInteger)cost {
+    [self _trimToCost:cost];
+}
+
+- (void)trimToAge:(NSTimeInterval)age {
+    [self _trimToAge:age];
+}
+
+
+- (NSString *)description {
+    if (_name) return [NSString stringWithFormat:@"<%@: %p> (%@)", self.class, self, _name];
+    return [NSString stringWithFormat:@"<%@: %p>", self.class, self];
+}
 
 @end
